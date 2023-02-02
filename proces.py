c1096bbb5d4bb2c2b7cd382ae553caaa4cddfb97
prpa <crispino@ucm.es>
2023-02-02 19:55:42 +0100

    1_procesos

--- proces.py
+++ proces.py
@@ -5,21 +5,22 @@ Created on Thu Feb  2 19:44:02 2023
 
 @author: prpa
 """
-
-from time import sleep
-from random import random
-"""
-DocumentaciÃ³n multiprocessing
-https://docs.python.org/3.6/library/multiprocessing.html
-"""
+import time
+import random
 from multiprocessing import Process
-def f():
-for i in range(5):
-print ("hola", i)
-sleep(random())
+def f(value):
+for i in range(3):
+print (f"hola soy {value} vuelta {i}")
+time.sleep(random.random()/3)
+def g():
+print ("adios")
 if __name__ == "__main__":
-p = Process(target=f)
-q = Process(target=f)
+N = 10
+lp = []
+for i in range(N):
+lp.append(Process(target=f,args=(f"ana {i}",)))
+for p in lp:
 p.start()
+q = Process(target=g)
 q.start()
 print ("fin")
